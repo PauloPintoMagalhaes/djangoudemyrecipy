5e1dc57ed71b803287a9c90bfd9a1b74bb833dc6
Paulo Magalhães <correio.paulomagalhaes@gmail.com>
2021-11-30 18:07:24 +0000

    fixed mistake on urls.py

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,15 +16,15 @@ class UserSerializer(serializers.ModelSerializer):
         """create a new user with encrtypted pass and returns the user"""
         return get_user_model().objects.create_user(**validated_data)
 
-    def update(self, validated_data):
-        """update a user, setting the password correctly and return it"""
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
         password = validated_data.pop('password', None)
         user = super().update(instance, validated_data)
 
         if password:
-            user.set_password(password) 
+            user.set_password(password)
             user.save()
-        
+
         return user
 
 
